2cdc0dd5099370dc163852103827263a2a9f1231
Roger Brooks <r.brooks@blackfinch.com>
2025-08-07 13:20:34 +0100

    Add subtitle

--- AttitudeQuestions.Domain/QuestionSet.cs
+++ AttitudeQuestions.Domain/QuestionSet.cs
@@ -3,8 +3,9 @@
 public class QuestionSet
 {
     public Guid Id { get; set; }
-    public required List<Question> Questions { get; set; }
     public required string Title { get; set; }
+    public required string SubTitle { get; set; }
+    public required List<Question> Questions { get; set; }
     public Guid? NextQuestionSetId { get; set; }
     public ChartData? ChartData { get; set; }
 }
